6609cad184803bf97a6627890bde3d4d01401d8c
Jack Meyers <mjmeyers@proton.me>
2022-07-20 14:16:23 -0500

    Navigation pt 2

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -3,6 +3,35 @@ import { View, Platform } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+	headerTintColor: '#fff',
+	headerStyle: {
+		backgroundColor: '#5637DD'
+	}
+}
+
+const HomeNavigator = _ => {
+	const Stack = createStackNavigator();
+
+	return (
+		<Stack.Navigator
+			screenOptions={screenOptions}
+		>
+			<Stack.Screen
+				name='Home'
+				component={HomeScreen}
+				options={
+					{ title: 'Home' }
+				}
+			/>
+		</Stack.Navigator>
+	);
+}
 
 const DirectoryNavigator = _ => {
 	const Stack = createStackNavigator();
@@ -10,12 +39,7 @@ const DirectoryNavigator = _ => {
 	return (
 		<Stack.Navigator
 			initialRouteName='Directory'
-			screenOptions={{
-				headerStyle: {
-					backgroundColor: '#5637DD'
-				},
-				headerTintColor: '#fff'
-			}}
+			screenOptions={screenOptions}
 		>
 			<Stack.Screen
 				name='Directory'
@@ -44,7 +68,27 @@ const Main = _ => {
 			flex: 1,
 			paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
 		}}>
-			<DirectoryNavigator />
+			<Drawer.Navigator
+				initialRouteName='Home'
+				drawerStyle={
+					{ backgroundColor: '#CEC8FF' }
+				}
+			>
+				<Drawer.Screen
+					name='Home'
+					component={HomeNavigator}
+					options={
+						{ title: 'Home' }
+					}
+				/>
+				<Drawer.Screen
+					name='Directory'
+					component={DirectoryNavigator}
+					options={{
+						title: 'Directory'
+					}}
+				/>
+			</Drawer.Navigator>
 		</View>
 	);
 };
