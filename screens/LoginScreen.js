12784039a66414dae09a72a09a4b5cab3edd9102
Jack Meyers <mjmeyers@proton.me>
2022-08-11 14:24:20 -0500

    Register Form

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -113,8 +113,115 @@ function LoginTab({ navigation }) {
 }
 
 function RegisterTab() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [remember, setRemember] = useState(false);
+
+	const handleRegister = _ => {
+		console.log(JSON.stringify({
+			username,
+			password,
+			firstName,
+			lastName,
+			email,
+			remember
+		}));
+		remember
+			&& SecureStore.setItemAsync(
+			'userinfo',
+			JSON.stringify({
+				username,
+				password
+			})).catch((error) => console.log('Could not save user info', error))
+			|| SecureStore.deleteItemAsync('userinfo')
+				.catch((error) => console.log('Could not delete user info', error));
+	}
+
 	return (
 		<ScrollView>
+			<View style={styles.container}>
+				<Input
+					placeholder='Username'
+					leftIcon={{
+						type: 'font-awesome',
+						name: 'user-o'
+					}}
+					onChangeText={text => setUsername(text)}
+					value={username}
+					containerStyle={styles.formInput}
+					leftIconContainerStyle={styles.formIcon}
+				/>
+				<Input
+					placeholder='Password'
+					leftIcon={{
+						type: 'font-awesome',
+						name: 'key'
+					}}
+					onChangeText={text => setPassword(text)}
+					value={password}
+					containerStyle={styles.formInput}
+					leftIconContainerStyle={styles.formIcon}
+				/>
+				<Input
+					placeholder='First Name'
+					leftIcon={{
+						type: 'font-awesome',
+						name: 'user-o'
+					}}
+					onChangeText={text => setFirstName(text)}
+					value={firstName}
+					containerStyle={styles.formInput}
+					leftIconContainerStyle={styles.formIcon}
+				/>
+				<Input
+					placeholder='Last Name'
+					leftIcon={{
+						type: 'font-awesome',
+						name: 'user-o'
+					}}
+					onChangeText={text => setLastName(text)}
+					value={lastName}
+					containerStyle={styles.formInput}
+					leftIconContainerStyle={styles.formIcon}
+				/>
+				<Input
+					placeholder='Email'
+					leftIcon={{
+						type: 'font-awesome',
+						name: 'envelope-o'
+					}}
+					onChangeText={text => setEmail(text)}
+					value={email}
+					containerStyle={styles.formInput}
+					leftIconContainerStyle={styles.formIcon}
+				/>
+				<CheckBox
+					title='Remember Me'
+					center
+					checked={remember}
+					onPress={_ => setRemember(!remember)}
+					containerStyle={styles.formCheckbox}
+				/>
+				<View style={styles.formButton}>
+					<Button
+						onPress={_ => handleRegister()}
+						title='Register'
+						color='#5637dd'
+						icon={
+							<Icon
+								name='user-plus'
+								type='font-awesome'
+								color='#fff'
+								iconStyle={{ marginRight: 10 }}
+							/>
+						}
+						buttonStyle={{ backgroundColor: '#5637dd' }}
+					/>
+				</View>
+			</View>
 		</ScrollView>
 	);
 }
@@ -165,20 +272,23 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
 	container: {
 		justifyContent: 'center',
-		margin: 20
+		margin: 10
 	},
 	formIcon: {
 		marginRight: 10
 	},
 	formInput: {
-		padding: 10
+		padding: 8,
+		height: 60
 	},
 	formCheckbox: {
-		margin: 10,
+		margin: 8,
 		backgroundColor: null
 	},
 	formButton: {
-		margin: 40
+		margin: 20,
+		marginRight: 40,
+		marginLeft: 40
 	}
 });
 
